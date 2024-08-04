8c9afb680d90cd92ae1a080d3da195578156bcd5
chetanmh20 <chetanmh20@gmail.com>
2024-08-04 11:17:12 +0530

    add catalog microservice and building blocks and docker support

---
+++ src/Services/Catalog/CatalogAPI/Exceptions/ProductNotFoundException.cs
@@ -0,0 +1,9 @@
+﻿using BuildingBlocks.Exceptions;
+
+namespace CatalogAPI.Exceptions;
+
+public class ProductNotFoundException: NotFoundException
+{
+    public ProductNotFoundException(Guid Id): base("Product", Id)
+    { }
+}
---
+++ src/Services/Catalog/CatalogAPI/Products/GetProducts/GetProductsEndpoint.cs
@@ -0,0 +1,24 @@
+﻿
+namespace CatalogAPI.Products.GetProducts;
+
+public record GetProductResponse(IEnumerable<Product> Products);
+
+public class GetProductsEndpoint : ICarterModule
+{
+    void ICarterModule.AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/products", async (ISender sender) =>
+        {
+            var result = await sender.Send(new GetProductsQuery());
+
+            var response = result.Adapt<GetProductResponse>();
+
+            return Results.Ok(response);
+        })
+            .WithName("GetProdutcs")
+            .Produces<GetProductResponse>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithSummary("Get Produtcs")
+            .WithDescription("Get Products");
+    }
+}
